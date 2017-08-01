4f31e94f672b25ad932f96a6718ffd0a4bdce2c2
Promode <luckydutta96@gmail.com>
2017-08-01 21:27:03 +0530

    Home screen bgcolor and components making

---
+++ app/components/Container/index.js
@@ -0,0 +1,3 @@
+import Container from './Container';
+import styles from './style';
+export { Container, styles };
--- app/index.js
+++ app/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Home from '../app/screens/Home';
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-    <Text>Changes you make will automatically reload.</Text>
-    <Text>Shake your phone to open the developer menu.</Text>
-
-    <Text>Test your phone to open the developer menu.</Text>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+EStyleSheet.build({
+  $primaryBlue: '#FF5722',
 });
 
-export default App;
+export default () => <Home />;
